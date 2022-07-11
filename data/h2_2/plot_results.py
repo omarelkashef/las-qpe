7932d02bc6bf03580d927f7fcf800edcf0b9c6ec
rdcunha <rdcunha2@vt.edu>
2022-07-11 10:17:46 -0500

    larger fontsize for h2_2 plots

--- data/h2_2/plot_results.py
+++ data/h2_2/plot_results.py
@@ -17,13 +17,23 @@ for frag in frag_list:
     for an in an_list:
         results = np.load('results_{}_{}.npy'.format(an, shots), allow_pickle=True).item()
         fig = plt.figure(figsize=(12,4.8))
-        plt.bar(range(len(results['phases'][frag])), list(results['phases'][frag].values()))
-        plt.xticks(range(len(results['phases'][frag])), list(results['phases'][frag].keys()), fontsize=16, rotation=45, ha="right")
-        if an == 6 or an == 8 or an == 10:
-            plt.xticks(color='w')
+        state_list = list(results['phases'][frag].keys())
+        prob_list = list(results['phases'][frag].values())
+        new_state_list = []
+        new_prob_list = []
+        for idx, prob in enumerate(prob_list):
+            if prob < 1e-4:
+                print("Prob {} too small".format(prob))
+            else:
+                new_prob_list.append(prob)
+                new_state_list.append(state_list[idx])
+        print(prob_list,new_prob_list)
+        print(state_list,new_state_list)
+        plt.bar(range(len(new_state_list)), new_prob_list)
+        plt.xticks(range(len(new_state_list)), new_state_list, fontsize=12, rotation=45, ha="right")
         plt.xlabel('Ancilla qubit states', fontsize=16)
         plt.ylabel('Probability', fontsize=16)
-        fig.savefig("{}_qubits_{}_shots_h2_{}.png".format(an, shots, frag), bbox_inches="tight")
+        fig.savefig("{}_qubits_{}_shots_h2_{}.png".format(an, shots, frag), bbox_inches="tight", dpi=300)
 
         most_likely_an = max(results['phases'][frag], key=results['phases'][frag].get)
         en_list.append(results['energies'][frag][most_likely_an])
@@ -52,7 +62,7 @@ plt.axhline(color='k')
 plt.xlabel('Number of ancilla qubits', fontsize=16)
 plt.ylabel('E [Hartree]', fontsize=16)
 plt.ylim(-0.4, 0.8)
-fig.savefig("en_{}_shots_h2_1.png".format(shots), bbox_inches="tight")
+fig.savefig("en_{}_shots_h2_1.png".format(shots), bbox_inches="tight", dpi=300)
 fig = plt.figure()
 plt.plot(an_list, err_list[6:], 'o-')
 plt.axhline(color='k')
@@ -60,4 +70,5 @@ plt.axhspan(-0.355, 0.355, facecolor='0.5', alpha=0.5)
 plt.xlabel('Number of ancilla qubits', fontsize=16)
 plt.ylabel('E-E(FCI) [Hartree]', fontsize=16)
 plt.ylim(-0.4, 0.8)
-fig.savefig("err_{}_shots_h2_1.png".format(shots), bbox_inches="tight")
+fig.savefig("err_{}_shots_h2_1.png".format(shots), bbox_inches="tight", dpi=300)
+
